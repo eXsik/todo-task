bccdf766534f935e1143b8368f7c7f8b53cb05f2
Kuba Skupinski <qba.skupinski@gmail.com>
2024-12-19 19:37:38 +0100

    fixed modal for deleting task

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -139,27 +139,32 @@
                                                         d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                 </svg>
                                             </button>
+
                                             <div x-show="showConfirmation" @click.away="showConfirmation = false" x-cloak
                                                 x-transition.opacity
-                                                class="mt-2 p-4 bg-red-100 rounded-md absolute right-4">
+                                                class="bg-black/50 absolute inset-0 flex flex-col items-center justify-center ">
 
-                                                <p>Czy na pewno chcesz usunąć zadanie <span
-                                                        class="truncate ...">{{ strlen($task->name) > 20 ? substr($task->name, 0, 17) . '...' : $task->name }}</span>?
-                                                </p>
-                                                <form action="{{ route('tasks.destroy', $task) }}" method="POST"
-                                                    class="mt-2">
-                                                    @csrf
-                                                    @method('DELETE')
+                                                <div class="mt-2 p-4 bg-red-100 rounded-md">
 
-                                                    <button type="submit"
-                                                        class="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-800">
-                                                        Tak, usuń
-                                                    </button>
-                                                    <button @click="showConfirmation = false" type="button"
-                                                        class="ml-2 text-gray-600 hover:text-gray-800">
-                                                        Anuluj
-                                                    </button>
-                                                </form>
+                                                    <p>Czy na pewno chcesz usunąć zadanie <span
+                                                            class="truncate ...">{{ strlen($task->name) > 20 ? substr($task->name, 0, 17) . '...' : $task->name }}</span>?
+                                                    </p>
+                                                    <form action="{{ route('tasks.destroy', $task) }}" method="POST"
+                                                        class="mt-2">
+                                                        @csrf
+                                                        @method('DELETE')
+
+                                                        <button type="submit"
+                                                            class="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-800">
+                                                            Tak, usuń
+                                                        </button>
+                                                        <button @click="showConfirmation = false" type="button"
+                                                            class="ml-2 text-gray-600 hover:text-gray-800">
+                                                            Anuluj
+                                                        </button>
+                                                    </form>
+                                                </div>
+
                                             </div>
                                         </div>
                                     </div>
